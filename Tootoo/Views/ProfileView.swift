68e3c93daddb2993ea5aaa7462201511b924e462
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-08-30 18:26:00 -0400

    log out!

--- Tootoo/Views/ProfileView.swift
+++ Tootoo/Views/ProfileView.swift
@@ -13,11 +13,48 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    VStack(alignment: .center) {
+                        Text(user.name)
+                            .bold()
+                            .font(.system(size: 22))
+                            .padding()
+                        HStack {
+                            Text("Email")
+                                .bold()
+                            Text (user.email)
+                        }
+                        HStack {
+                            Text("Member since")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .omitted))")
+                        }
+                        Button() {
+                            viewModel.signOut()
+                        } label: {
+                            Text("Sign Out")
+                                .bold()
+                        }
+                        .tint(Color.red)
+                        .padding()
+                    }
+                } else {
+                    Text("Loading profile...")
+                }
             }
             .navigationTitle("Account")
         }
+        .onAppear {
+                viewModel.fetchUser()
+            }
     }
+    
 }
 
 struct ProfileView_Previews: PreviewProvider {
